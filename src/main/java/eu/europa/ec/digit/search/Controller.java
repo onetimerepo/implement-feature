e51b9ae3cc056d62f105094adcb877d5a4435df1
vasily.tsvetkov <vasily.tsvetkov@multiloginapp.com>
2021-03-15 18:01:40 +0200

    Mb POST is better for the DTO input

--- src/main/java/eu/europa/ec/digit/search/Controller.java
+++ src/main/java/eu/europa/ec/digit/search/Controller.java
@@ -1,7 +1,7 @@
 package eu.europa.ec.digit.search;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +10,8 @@ public class Controller {
 
     }
 
-    @GetMapping("/sort")
-    public String doSort(@PathVariable DTO dto) {
+    @PostMapping("/sort")
+    public String doSort(@RequestBody DTO dto) {
         return "foo";
     }
 }
